a25aa941f6c6d4faad785c2b5c6ac493b491529b
Almond Kiruthu <113090717+almondkiruthu@users.noreply.github.com>
2025-02-05 21:48:20 +0300

    Created 'Presents' by A2SV Hub

---
+++ 05-Feb-2025/Presents 244889.py
@@ -0,0 +1,16 @@
+# Problem: Presents - https://codeforces.com/problemset/problem/136/A
+
+# ith number is Pi -- the number of a friend who gave a gift to friend i
+# each friend received at least one gift
+# possible that some friends don't give
+
+
+T = int(input())
+gifts = [int(i) for i in input().split()]
+gift_map = {}
+for giver in range(T):
+    receiver = gifts[giver]
+    gift_map[receiver] = giver + 1
+
+sorted_givers = [gift_map[key] for key in sorted(gift_map)]
+print(" ".join([str(giver) for giver in sorted_givers]))
