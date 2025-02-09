84facfc7431271c28c317419e0938c325a7660b2
Almond Kiruthu <113090717+almondkiruthu@users.noreply.github.com>
2025-02-09 23:37:16 +0300

    Created 'Same Differences' by A2SV Hub

---
+++ 09-Feb-2025/Same Differences 252518.py
@@ -0,0 +1,23 @@
+# Problem: Same Differences - https://codeforces.com/problemset/problem/1520/D
+
+T = int(input())
+
+
+while (T > 0):
+    n = int(input())
+    if n > 0:
+        count = 0
+        freq_map = {}
+        nums = [int(_) for _ in input().split()]
+        for i in range(n):
+            num = nums[i]
+            diff = num - i
+            if diff in freq_map:
+                # take the prev count
+                prev_count = freq_map[diff]
+                count += prev_count
+                freq_map[diff] += 1
+            else:
+                freq_map[diff] = 1
+        print(count)
+    T -= 1
