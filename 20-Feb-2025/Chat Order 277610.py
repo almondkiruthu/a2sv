dc1ce86c8a001eba4321a0e29c22cad8f29fae35
Almond Kiruthu <113090717+almondkiruthu@users.noreply.github.com>
2025-02-20 23:42:51 +0300

    Created 'Chat Order' by A2SV Hub

---
+++ 20-Feb-2025/Chat Order 277610.py
@@ -0,0 +1,17 @@
+# Problem: Chat Order - https://codeforces.com/problemset/problem/637/B
+
+from collections import OrderedDict
+
+
+testcases = int(input())
+
+
+chat_list = OrderedDict()
+for _ in range(testcases):
+    friend = input()
+    if friend in chat_list:
+        chat_list.pop(friend)  # Remove from current position
+    chat_list[friend] = None # Insert at the end (0(1))
+
+for friend in reversed(chat_list):
+    print(friend)
