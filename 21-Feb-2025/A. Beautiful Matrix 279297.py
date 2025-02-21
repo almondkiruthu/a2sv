eb1d1016025a8b1132fe253edd2fd7c780be9f35
Almond Kiruthu <113090717+almondkiruthu@users.noreply.github.com>
2025-02-21 21:07:43 +0300

    Created 'A. Beautiful Matrix' by A2SV Hub

---
+++ 21-Feb-2025/A. Beautiful Matrix 279297.py
@@ -0,0 +1,12 @@
+# Problem: A. Beautiful Matrix - https://codeforces.com/problemset/problem/263/A
+
+x2, y2 = 0, 0
+
+for i in range(5):
+  row = [int(_) for _ in input().split()]
+  if 1 in row:
+    x2, y2 = i, row.index(1) # get row index and column index
+    break
+  
+moves = abs(x2 - 2) + abs(y2 - 2)
+print(moves)
