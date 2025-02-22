fe6bf64b335d8dc5aa60f92887b68f84a17fe1c3
Almond Kiruthu <113090717+almondkiruthu@users.noreply.github.com>
2025-02-22 23:31:28 +0300

    Created 'Gas Station' by A2SV Hub

---
+++ 22-Feb-2025/Gas Station 280597.py
@@ -0,0 +1,19 @@
+# Problem: Gas Station - https://leetcode.com/problems/gas-station/
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+        
+        total = 0
+        
+        # start position
+        start = 0
+        for i in range(len(gas)):
+            # what's the differnce between gas and the cost.
+            total += (gas[i] - cost[i])
+            if total < 0:
+                total = 0
+                start = i + 1
+        
+        return start
