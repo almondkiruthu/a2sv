e8323162dbf04a3e86a2c4900ca783847c4da9bf
Almond Kiruthu <113090717+almondkiruthu@users.noreply.github.com>
2025-02-24 23:09:08 +0300

    Created 'Fox And Snake' by A2SV Hub

---
+++ 24-Feb-2025/Fox And Snake 282052.py
@@ -0,0 +1,15 @@
+# Problem: Fox And Snake - https://codeforces.com/problemset/problem/510/A
+
+rows, cols = [int(_) for _ in input().split()]
+
+for row in range(rows):
+    # note that it's a zero based index language
+    # odd rows for zero based index
+    if row % 2 == 0:
+        print("#" * cols)
+    # even row that follow an odd row, put # at the rightmost
+    elif row % 4 == 1:
+        print("." * (cols - 1) + "#")
+    # even row that follows another even row, put # at the leftmost
+    else:
+        print("#" + "." * (cols - 1))
