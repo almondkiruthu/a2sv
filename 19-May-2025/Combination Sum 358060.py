8aad67a1be8dcceb468107b4bbb956fb53e40857
Almond Kiruthu <113090717+almondkiruthu@users.noreply.github.com>
2025-05-19 19:10:41 +0300

    Created 'Combination Sum' by A2SV Hub

---
+++ 19-May-2025/Combination Sum 358060.py
@@ -0,0 +1,27 @@
+# Problem: Combination Sum - https://leetcode.com/problems/combination-sum/
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        def dfs(i, cur, total):
+            if total == target:
+                res.append(cur.copy())
+
+                return
+            if i >= len(candidates) or total > target:
+                return
+            
+            #Left choice include candidate
+            cur.append(candidates[i])
+            dfs(i, cur, total + candidates[i])
+            cur.pop()
+
+            #Right choice don't include
+            dfs(i + 1, cur, total)
+
+        dfs(0, [], 0)
+
+        return res
+
+        
