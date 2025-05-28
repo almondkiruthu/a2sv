7d760b61e9cde191ee86f545d02282af402efc70
Almond Kiruthu <113090717+almondkiruthu@users.noreply.github.com>
2025-05-28 23:21:25 +0300

    Created 'Climbing Stairs' by A2SV Hub

---
+++ 28-May-2025/Climbing Stairs 360610.py
@@ -0,0 +1,21 @@
+# Problem: Climbing Stairs - https://leetcode.com/problems/climbing-stairs/
+
+class Solution:
+    def climbStairs(self, n: int) -> int: 
+        cache = {}
+        def dfs(i):
+            if i == n:
+                return 1
+            elif i > n:
+                return 0
+            
+            # memoization if i in cache then we have already calculated the ways from this step "i"
+            if i in cache:
+                return cache[i]
+            # Recursive step: The number of ways to reach "n" from starting point i
+            # Is the sum of ways to reach n from the i + 1 branch and i + 2 branch
+            path_count = dfs(i + 1) + dfs(i + 2)
+            cache[i] = path_count
+            return path_count
+
+        return dfs(0)
